d66af672eae09dad103f72803d5439d50a13ee1a
justastranger <justastrangerlica@gmail.com>
2024-03-21 14:37:44 -0500

    Add project files.

---
+++ TimeToLive/Config.cs
@@ -0,0 +1,15 @@
+﻿using StardewModdingAPI;
+
+namespace TimeToLive
+{
+    internal class Config
+    {
+        public SButton debugKey { get; set; }
+        public int lifespan { get; set; }
+
+        public Config()
+        {
+            lifespan = 7;
+        }
+    }
+}
---
+++ TimeToLive/ModEntry.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace TimeToLive
+{
+    public class ModEntry : Mod
+    {
+        internal Config config;
+        internal ITranslationHelper i18n => Helper.Translation;
+
+        public override void Entry(IModHelper helper)
+        {
+            string startingMessage = i18n.Get("TimeToLive.start", new { mod = helper.ModRegistry.ModID, folder = helper.DirectoryPath });
+            Monitor.Log(startingMessage, LogLevel.Trace);
+
+            config = helper.ReadConfig<Config>();
+
+        }
+    }
+
+    [HarmonyPatch(typeof(NPC), "draw", new Type[]
+    {
+        typeof(SpriteBatch),
+        typeof(float)
+    })]
+    public static class patch
+    {
+
+    }
+}
